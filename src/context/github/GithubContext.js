8b3261c3cf4d6b92e200df77460620a8e9547f04
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-11-08 14:42:19 -0300

    01 - Move SearchUsers to Actions File

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,45 +16,6 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get initial users (testing purposes)
-  const fetchUsers = async () => {
-    setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await response.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: data,
-    });
-  };
-
-  // Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const { items } = await response.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    });
-  };
-
   const getUser = async (login) => {
     setLoading();
 
@@ -107,12 +68,8 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        fetchUsers,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
         getRepos,
